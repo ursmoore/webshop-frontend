70fa723dedcf14fd53abbdd277010fab76d25a9a
ursmoore <urs.moore@gmx.de>
2022-02-08 13:12:12 +0100

    putted Courier as font family in styles.css container

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,58 @@
 // navbar index.js
 import styles from "./styles.css";
 import { NavLink } from "react-router-dom";
-import { GiShoppingCart, GiHearts } from "react-icons/gi";
+import { FiShoppingCart, FiHeart, FiUser } from "react-icons/fi";
 
 const NavBar = () => {
   return (
     <div class="container">
-      <div class="green"></div>
-      <NavLink
-        to="/shop"
-        className="links"
-        style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
-      ></NavLink>
-      <div class="red"></div>
-      <div class="icons">
+      <div class="green">
+        <NavLink
+          to="/shop"
+          className="links"
+          style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
+        >
+          TheShop Home Product Blog Shop Contact
+        </NavLink>
+      </div>
+      <div class="red">
+        <NavLink
+          to="/shop"
+          className="links"
+          style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
+        >
+          SEARCH
+        </NavLink>
+      </div>
+      <div class="iconsBar">
         <div>
-          <GiShoppingCart />
-          <GiHearts />
+          <NavLink
+            to="/"
+            className="icons"
+            style={({ isActive }) =>
+              isActive ? { color: "black" } : undefined
+            }
+          >
+            <FiShoppingCart />
+          </NavLink>
+          <NavLink
+            to="/"
+            className="icons"
+            style={({ isActive }) =>
+              isActive ? { color: "black" } : undefined
+            }
+          >
+            <FiHeart />
+          </NavLink>
+          <NavLink
+            to="/"
+            className="icons"
+            style={({ isActive }) =>
+              isActive ? { color: "black" } : undefined
+            }
+          >
+            <FiUser />
+          </NavLink>
         </div>
       </div>
     </div>
