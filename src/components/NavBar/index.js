33be35353860d3d8c0ed0392be8dd52dfa506d6d
ursmoore <urs.moore@gmx.de>
2022-02-08 12:09:52 +0100

    icons in NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,17 +1,24 @@
 // navbar index.js
 import styles from "./styles.css";
 import { NavLink } from "react-router-dom";
+import { GiShoppingCart, GiHearts } from "react-icons/gi";
 
 const NavBar = () => {
   return (
     <div class="container">
       <div class="green"></div>
-      <div class="red"></div>
       <NavLink
         to="/shop"
         className="links"
         style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
       ></NavLink>
+      <div class="red"></div>
+      <div class="icons">
+        <div>
+          <GiShoppingCart />
+          <GiHearts />
+        </div>
+      </div>
     </div>
   );
 };
